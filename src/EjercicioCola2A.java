44eaa29303ab7d5b648be071947bae30b98f90e1
Ruru <121054548+LucasCap12@users.noreply.github.com>
2024-04-09 14:23:15 -0300

    Actualizacion pre 090424

---
+++ src/EjercicioCola2A.java
@@ -0,0 +1,5 @@
+public class EjercicioCola2A {
+    public static void main(String[] args) throws Exception {
+        
+}
+}
---
+++ src/method/PasarCola.java
@@ -0,0 +1,13 @@
+package method;
+
+import api.ColaTDA;
+
+public class PasarCola {
+    public static void PasaCola(ColaTDA origen, ColaTDA destino){
+        while (!c.ColaVacia()) {
+            destino.Acolar(origen.Primero());
+            destino.Desacolar();
+        }
+    }
+
+}
